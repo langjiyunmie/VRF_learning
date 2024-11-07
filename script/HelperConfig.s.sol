6ddfcf987604c96d6fddbc0f672b685e68a99ea4
langjiyunmie <1449866158@qq.com>
2024-11-07 16:10:36 +0800

    Initial commit 2

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -107,7 +107,7 @@ contract HelperConfig is CodeConstants, Script {
 
         console2.log(unicode"⚠️ You have deployed a mock conract!");
         console2.log("Make sure this was intentional");
-        vm.startBroadcast();
+        vm.startBroadcast(FOUNDRY_DEFAULT_SENDER);
         VRFCoordinatorV2_5Mock vrfCoordinatorV2_5Mock =
             new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UINT_LINK);
         LinkToken link = new LinkToken();
